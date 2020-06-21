7aa21a7c48ee062542707657160a5fd453ee4511
caizr <0@caizr.com>
2020-06-22 01:34:07 +0800

    a

--- C5/2_bin_r_w.cpp
+++ C5/2_bin_r_w.cpp
@@ -10,23 +10,26 @@ struct Student{
     string name;
     int age;
     int score;
+    Student(){};
     Student(string n,int a,int s): name(n),age(a),score(s){};
     ~Student(){
         cout<<"delete"<<endl;
     }
 };
 int main(){
-   Student s1("petter",12,100);
+  Student s1("petter",12,100);
    // 创建读取流
    FILE * f1=fopen("2_bin_files.txt","w+");
    // 输写入的字节数
-   cout<<"write "<<fwrite(&s1,sizeof(s1),sizeof(s1),f1)<<" bytes"<<endl;;
-   fclose(f1);
+  cout<<"write "<<fwrite(&s1,sizeof(s1),sizeof(s1),f1)<<" bytes"<<endl;
    // 创建新的读取流
-   FILE * f2=fopen("2_bin_files.txt","r");
-   Student *s2;
+   FILE * f2=fopen("2_bin_file.txt","r");
+   Student *s2 = new Student{"a",0,0};
    // 输出读取的字节数
-   cout<<"read "<<fread(&s2,sizeof(s2),sizeof(s2),f2)<<" bytes"<<endl;
+   cout<<"read "<<fread(s2,sizeof(s2),40,f2)<<" bytes"<<endl;
+    cout<<s2->age<<endl;
+    //fclose(f1);
+  // fclose(f1);
    fclose(f2);
    return 0;
 }
