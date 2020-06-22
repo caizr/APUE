786b99d08799e62a3a96c8908e5f98b7dc2705df
caizr <0@caizr.com>
2020-06-22 20:53:47 +0800

    阿

--- C5/5_temp_file.c
+++ C5/5_temp_file.c
@@ -9,9 +9,17 @@ int main(){
     c=tmpnam(b);
     printf("%s\n",c);
 
-    // 使用 mkdtemp(),mkstemp()
-    char *d="filexxxxxx";
-    d=mkdtemp(d);
-    printf("%s\n",d);
+    /***** 使用 mkdtemp(),mkstemp()  *****/
+    // mkdtemp()创建临时目录
+    // 使用char *d 的时候报错
+    char d[]="/tmp/XXXXXX";
+    char *dir_name=mkdtemp(d);      // d[]现在也会被改变
+    printf("%s\n",dir_name);
+    
+    // mkstemp()创建临时文件
+    // 使用char *e 的时候报错 
+    char e[] = "fileXXXXXX";        // e[]同样会被改变
+    mkstemp(e);
+    printf("%s\n",e);
     return 0;
 }
